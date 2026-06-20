4953b3e1e674726891bc55bdcf1600b0b42673a4
Anton Krivenko <antonkrivenko75@gmail.com>
2026-06-20 17:25:40 +0300

    new variable

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Hello World")
-	fmt.Printf(", I'm Anton")
+	userHeight := 1.8
+	var userKg float64
+	userKg = 70
+	var IMT = userKg / math.Pow(userHeight, 2)
+	fmt.Print(IMT)
+	if IMT > 30 {
+		fmt.Print(" You're fat")
+	}
+	if IMT < 18 {
+		fmt.Print(" You're slime")
+	}
+	if IMT > 18.5 {
+		fmt.Print("You're a nice guy")
+	}
 }
