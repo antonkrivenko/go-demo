fcc7933c59ce403ac245ac0f4ebb6076f0d960c5
Anton Krivenko <antonkrivenko75@gmail.com>
2026-06-23 14:45:58 +0300

    add scan

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
-	userHeight := 1.8
+	const IMTPower = 2
+	var userHeight float64
 	var userKg float64
-	userKg = 70
-	var IMT = userKg / math.Pow(userHeight, 2)
+	fmt.Print("--- Калькулятор індекса маси тіла ---\n")
+	fmt.Print("Введіть свій ріст в метрах ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введіть свою вагу ")
+	fmt.Scan(&userKg)
+	IMT := userKg / math.Pow(userHeight, IMTPower)
+	fmt.Print("Ваш індекс маси тіла: ")
 	fmt.Print(IMT)
 	if IMT > 30 {
 		fmt.Print(" You're fat")
